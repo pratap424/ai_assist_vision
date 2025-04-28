35f83eb730bee250e1511ae3aec6ca845b3d69fc
Yash Pratap Singh Rathore <64972796+pratap424@users.noreply.github.com>
2025-04-28 19:24:01 +0530

    Update vlm_captioning.py

--- modules/vlm_captioning.py
+++ modules/vlm_captioning.py
@@ -1,12 +1,13 @@
 from transformers import BlipProcessor, BlipForConditionalGeneration
 from PIL import Image
 
-
-def describe_scene(image_path):
-    blip_processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
-    blip_model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+blip_processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+blip_model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
     image = Image.open(image_path).convert("RGB")
+
+def describe_scene(image_path):
+  
     inputs = blip_processor(images=image, return_tensors="pt")
     out = blip_model.generate(**inputs)
     return blip_processor.decode(out[0], skip_special_tokens=True)
